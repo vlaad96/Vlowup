140f7bc3b4429eaeec22bb1f9a5984ef339fb84e
Pol Bosch <pol.hcsob@gmail.com>
2018-10-08 17:48:20 +0200

    our maps folder

---
+++ Assignment1/Game/maps/Our maps/_Spritesheet.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="_Spritesheet" tilewidth="256" tileheight="256" spacing="1" margin="1" tilecount="36" columns="6">
+ <image source="../PNG/_Spritesheet.png" width="1792" height="1792"/>
+ <terraintypes>
+  <terrain name="Ground" tile="7"/>
+  <terrain name="Platform" tile="19"/>
+  <terrain name="Column" tile="3"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,0"/>
+ <tile id="3" terrain="2,2,2,2"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="2,2,2,2"/>
+ <tile id="12" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="2,2,2,2"/>
+ <tile id="18" terrain="1,1,1,1"/>
+ <tile id="19" terrain="1,1,1,1"/>
+ <tile id="20" terrain="1,1,1,1"/>
+</tileset>
