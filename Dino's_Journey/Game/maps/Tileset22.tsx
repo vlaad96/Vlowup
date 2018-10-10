7a1ae4c0973e321c488004b21a8c7a5450a94203
Vladislav Bulantsev <vladislavbv96@gmail.com>
2018-10-10 19:06:47 +0200

    renamed the game

---
+++ Dino's_Journey/Game/maps/Tileset22.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset22" tilewidth="64" tileheight="64" tilecount="144" columns="16">
+ <image source="Tileset22.png" width="1024" height="576"/>
+ <terraintypes>
+  <terrain name="Nuevo Terreno" tile="17"/>
+  <terrain name="Nuevo Terreno" tile="23"/>
+  <terrain name="Nuevo Terreno" tile="64"/>
+ </terraintypes>
+ <tile id="0" terrain="2,2,2,0"/>
+ <tile id="1" terrain="2,2,0,0"/>
+ <tile id="2" terrain="2,2,0,2"/>
+ <tile id="3" terrain="0,0,0,2"/>
+ <tile id="4" terrain="0,0,2,2"/>
+ <tile id="5" terrain="0,0,2,0"/>
+ <tile id="6" terrain="2,2,2,1"/>
+ <tile id="7" terrain="2,2,1,1"/>
+ <tile id="8" terrain="2,2,1,2"/>
+ <tile id="9" terrain="1,1,1,2"/>
+ <tile id="10" terrain="1,1,2,2"/>
+ <tile id="11" terrain="1,1,2,1"/>
+ <tile id="16" terrain="2,0,2,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,2,0,2"/>
+ <tile id="19" terrain="0,2,0,2"/>
+ <tile id="21" terrain="2,0,2,0"/>
+ <tile id="22" terrain="2,1,2,1"/>
+ <tile id="23" terrain="1,1,1,1"/>
+ <tile id="24" terrain="1,2,1,2"/>
+ <tile id="25" terrain="1,2,1,2"/>
+ <tile id="27" terrain="2,1,2,1"/>
+ <tile id="32" terrain="2,0,2,2"/>
+ <tile id="33" terrain="0,0,2,2"/>
+ <tile id="34" terrain="0,2,2,2"/>
+ <tile id="35" terrain="0,2,0,0"/>
+ <tile id="36" terrain="2,2,0,0"/>
+ <tile id="37" terrain="2,0,0,0"/>
+ <tile id="38" terrain="2,1,2,2"/>
+ <tile id="39" terrain="1,1,2,2"/>
+ <tile id="40" terrain="1,2,2,2"/>
+ <tile id="41" terrain="1,2,1,1"/>
+ <tile id="42" terrain="2,2,1,1"/>
+ <tile id="43" terrain="2,1,1,1"/>
+ <tile id="64" terrain="2,2,2,2"/>
+</tileset>
