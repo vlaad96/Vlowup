95f7f2b4d6aa4c82d39bf03e1ade6144f0b121b3
Vladislav Bulantsev <vladislavbv96@gmail.com>
2018-10-14 19:57:39 +0200

    useless folders

--- Assignment1/Game/maps/_Spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="_Spritesheet" tilewidth="32" tileheight="32" tilecount="3136" columns="56">
- <image source="_Spritesheet.png" width="1792" height="1792"/>
-</tileset>
